ac04d2094cb12b93f31f965e3c4c5808fae805d1
Kurt Reed <kurtareed@gmail.com>
2018-07-29 16:22:47 -0600

    Did chapter 14 exercise 8

---
+++ chapter14/exercise8/my_io.h
@@ -0,0 +1,11 @@
+//
+// Created by Kurt on 7/27/2018.
+//
+
+#ifndef EXERCISE8_MY_IO_H
+#define EXERCISE8_MY_IO_H
+
+#endif //EXERCISE8_MY_IO_H
+
+void eatline(void);
+char * my_gets(char *string, int n);
