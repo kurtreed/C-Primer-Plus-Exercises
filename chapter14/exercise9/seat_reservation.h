6a4dde8331e1b1548a4f90b6418d51198f8bfa19
Kurt Reed <kurtareed@gmail.com>
2018-07-29 16:23:15 -0600

    Did chapter 14 exercise 9

---
+++ chapter14/exercise9/seat_reservation.h
@@ -0,0 +1,22 @@
+//
+// Created by Kurt on 7/27/2018.
+//
+
+#ifndef EXERCISE8_SEAT_RESERVATION_H
+#define EXERCISE8_SEAT_RESERVATION_H
+
+#endif //EXERCISE8_SEAT_RESERVATION_H
+
+struct seat_t{
+    unsigned int id;
+    int assigned; // 1 if it is assigned, 0 if not.
+    char first_name[20];
+    char last_name[20];
+};
+typedef struct seat_t seat;
+
+struct plane_t{
+    struct seat_t seat[12];
+    int flightnum;
+};
+typedef struct plane_t plane;
